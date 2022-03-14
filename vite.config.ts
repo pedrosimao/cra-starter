400ac65f358cc04ca24153d3ab7cc727f37a8b3b
Pedro Simão <pedrolimao@gmail.com>
2022-03-14 09:48:45 +0100

    Allow emotion css prop out of the box

--- vite.config.ts
+++ vite.config.ts
@@ -24,12 +24,13 @@ export default defineConfig({
   plugins: [
     react(
       {
+        jsxImportSource: "@emotion/react",
         babel: {
           plugins: [
             'babel-plugin-macros',
             // 'macros',
             [
-              "@emotion",
+              "@emotion/babel-plugin",
               {
                 "sourceMap": true,
                 "autoLabel": "dev-only",
